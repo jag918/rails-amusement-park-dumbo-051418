f42aed48ebbeb3a6da809f1a97e3f5df88914120
Jee Graeff <jag918@nyu.edu>
2018-06-14 16:35:32 -0400

    fixing the migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  # User
+  #   is valid with a name, password, happiness, nausea, height, and tickets (FAILED - 55)
+  #   is not valid without a password (FAILED - 56)
+  #   is valid with an admin boolean (FAILED - 57)
+  #   defaults to admin => false (FAILED - 58)
+  #   has many rides (FAILED - 59)
+  #   has many attractions through rides (FAILED - 60)
+  #   has a method 'mood' that returns 'sad' when the user is more nauseous than happy (FAILED - 61)
+  #   has a method 'mood' that returns 'happy' when the user is more happy than nauseous (FAILED - 62)
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  validates :name, :password, :happiness, :nausea, :height, :tickets, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  get '/home', to: 'application#home', as: 'root'
+
+  resources :users, only: [:new, :show, :index, :create]
 end
